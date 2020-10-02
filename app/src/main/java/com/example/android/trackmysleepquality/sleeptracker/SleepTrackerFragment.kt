e6d7e5a965f387261ead1aa3cba0bd6ba3162824
rukkachou <rukka.chou@gmail.com>
2020-10-02 17:36:36 +0800

    02(ch5) exercise finished: implement onCreateViewHolder and connect to Fragment & ViewModel

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -97,6 +97,15 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         return binding.root
     }
 }
